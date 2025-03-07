6da682ec3a836fd659cc8c00f771f97e20115220
Burak <burakkocamaz@hotmail.com>
2025-03-07 21:14:08 +0300

    perfect Number Calculate

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Let's ask the user for a number
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bir sayı giriniz :");
         int sayi = scanner.nextInt();
         int sum = 0;
+        //Let's check that our number is positive and the given number is a perfect number.
         if (sayi > 0) {
             for (int i = 1; i < sayi; i++) {
 
